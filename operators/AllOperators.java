92dcf1cea2cbeca3d6b9fb362ec42691b54a9cc1
Anushka Kharmale <anushkakharmale16@gmail.com>
2026-05-26 13:20:47 +0530

    Added Java operators program

---
+++ operators/AllOperators.java
@@ -0,0 +1,46 @@
+package operators;
+
+public class AllOperators {
+     public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+
+        // Arithmetic Operators
+        System.out.println("Arithmetic Operators");
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Modulus: " + (a % b));
+
+        // Assignment Operators
+        int x = 20;
+        System.out.println("\nAssignment Operators");
+        x += 5;
+        System.out.println("x += 5 : " + x);
+
+        x -= 2;
+        System.out.println("x -= 2 : " + x);
+
+        x *= 3;
+        System.out.println("x *= 3 : " + x);
+
+        // Relational Operators
+        System.out.println("\nRelational Operators");
+        System.out.println("a == b : " + (a == b));
+        System.out.println("a != b : " + (a != b));
+        System.out.println("a > b : " + (a > b));
+        System.out.println("a < b : " + (a < b));
+        System.out.println("a >= b : " + (a >= b));
+        System.out.println("a <= b : " + (a <= b));
+
+        // Logical Operators
+        System.out.println("\nLogical Operators");
+        System.out.println((a > b) && (a != b));
+        System.out.println((a < b) || (a != b));
+        System.out.println(!(a < b));
+     }        
+
+    
+}
