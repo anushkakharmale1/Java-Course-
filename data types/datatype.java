55e6ebeb4ac8950ad3768696ae6c3d8d7600234e
Anushka Kharmale <anushkakharmale16@gmail.com>
2026-05-27 09:29:36 +0530

    Added Java data types program

---
+++ data types/datatype.java
@@ -0,0 +1,18 @@
+public class datatype {
+     public static void main(String[] args)
+      {
+
+        int age = 20;
+        double marks = 95.5;
+        char grade = 'A';
+        boolean isJavaFun = true;
+        String name = "Anushka";
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(marks);
+        System.out.println(grade);
+        System.out.println(isJavaFun);
+    }
+    
+}
